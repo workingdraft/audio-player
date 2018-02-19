ae20f437cf3d2d66134d88809678d0ab7ad01315
Hans Christian Reinl <info@drublic.de>
2018-02-19 18:01:53 +0100

    Add player css

--- __tests__/visual/server.js
+++ __tests__/visual/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 
 class App {
@@ -5,6 +6,18 @@ class App {
     this.express = express()
 
     this.express.use('/', express.static('__tests__/visual/'))
+
+    this.mountRoutes()
+  }
+
+  mountRoutes() {
+    const router = express.Router()
+    const cssPath = path.join(__dirname, '../../', 'player.css')
+
+    router.get('/player.css', (req, res) => res.sendFile(cssPath))
+
+    // Use the router
+    this.express.use('/', router)
   }
 }
 
