6e25345f0a0c70e7c521007e1407bf79a7754dfc
Hans Christian Reinl <info@drublic.de>
2018-06-11 23:20:56 +0200

    Add export type as library, UMD

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -12,6 +12,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
+    library: '[name]',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [{
